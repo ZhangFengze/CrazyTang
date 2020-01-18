2396258192c13d18dfc2fe540ac7f15e5126a544
crazyZhang <917724035@qq.com>
2020-01-18 14:57:06 +0800

    test player assets

--- source/Game.cpp
+++ source/Game.cpp
@@ -3,15 +3,18 @@
 
 Game::Game()
 {
-	m_Player.SetFrameInterval(1.f / 5.f);
+	m_Bomb.SetFrameInterval(1.f / 5.f);
+	m_Player.SetFrameInterval(1.f / 10.f);
 }
 
 void Game::Update(float dt)
 {
+	m_Bomb.Update(dt);
 	m_Player.Update(dt);
 }
 
 void Game::Render(Renderer &r)
 {
+	m_Bomb.Render(r);
 	m_Player.Render(r);
 }
--- source/Game.h
+++ source/Game.h
@@ -10,5 +10,7 @@ public:
 	void Render(Renderer&);
 
 private:
-	AnimatedSprite m_Player = AnimatedSprite::Create({ "bomb/b1.bmp","bomb/b2.bmp","bomb/b3.bmp" });
+	AnimatedSprite m_Bomb = AnimatedSprite::Create({ "bomb/b1.jpg","bomb/b2.jpg","bomb/b3.jpg" });
+	AnimatedSprite m_Player = AnimatedSprite::Create(
+		{ "actor/1.jpg","actor/2.jpg","actor/3.jpg","actor/4.jpg" });
 };
