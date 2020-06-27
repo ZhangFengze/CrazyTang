b67bc1fddfd25e6f6c54cff6b1994cfb08820038
crazyZhang <917724035@qq.com>
2020-06-27 20:44:12 +0800

    acceptor wrapper

---
+++ common/source/Acceptor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <asio.hpp>
+
+namespace ct
+{
+	class Acceptor
+	{
+	public:
+		Acceptor(asio::io_context& io, asio::ip::tcp::endpoint endpoint,
+			std::function<void(const asio::error_code&, asio::ip::tcp::socket&&)> callback);
+
+	private:
+		void StartAccept(std::function<void(const asio::error_code&, asio::ip::tcp::socket&&) > callback);
+
+	public:
+		std::shared_ptr<bool> alive_ = std::make_shared<bool>();
+		asio::ip::tcp::acceptor acceptor_;
+	};
+}
