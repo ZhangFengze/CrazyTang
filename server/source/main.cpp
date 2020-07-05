891250d346632e1e62f6a1b80ddfa41135958372
crazyZhang <917724035@qq.com>
2020-07-05 22:14:08 +0800

    simple broadcast server

--- server/source/main.cpp
+++ server/source/main.cpp
@@ -1,7 +1,12 @@
 #ifndef CT_TEST
+#include "Server.h"
 
 int main()
 {
+	asio::io_context io;
+	auto endpoint = asio::ip::tcp::endpoint{ asio::ip::make_address("127.0.0.1"),3377 };
+	ct::BroadcastServer server{ io,endpoint };
+	io.run();
 	return 0;
 }
 
