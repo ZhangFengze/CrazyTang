2af76f2b08925d9c2406338c828653e3a4f5ecda
crazyZhang <917724035@qq.com>
2020-07-15 22:04:21 +0800

    rearrange Connection members' order

--- common/source/Connection.h
+++ common/source/Connection.h
@@ -13,10 +13,9 @@ namespace ct
 
 		void AsyncReadPacket(std::function<void(const std::error_code&, const char*, size_t)>&& handler);
 
-		asio::ip::tcp::socket socket_;
-
-	private:
+	public:
 		std::shared_ptr<bool> valid_ = std::make_shared<bool>(true);
+		asio::ip::tcp::socket socket_;
 		std::array<char, 1024 * 1024> buffer_;
 	};
 }
