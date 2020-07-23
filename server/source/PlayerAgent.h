dec87a8c26afb9351ae9da205c76d1326296ac80
crazyZhang <917724035@qq.com>
2020-07-23 23:34:06 +0800

    player agent

---
+++ server/source/PlayerAgent.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <string>
+#include <functional>
+#include "../../common/source/Packet.h"
+#include "../../common/source/Serializer.h"
+
+namespace ct
+{
+	template<typename Pipe>
+	class PlayerAgent
+	{
+	public:
+		PlayerAgent(std::shared_ptr<Pipe>);
+		void Listen(const std::string& tag, std::function<void(std::string&&)>);
+
+	private:
+		std::shared_ptr<Pipe> pipe_;
+		std::unordered_map<std::string, std::function<void(std::string&&)>> listeners_;
+	};
+
+	template<typename Pipe>
+	PlayerAgent<Pipe>::PlayerAgent(std::shared_ptr<Pipe> pipe)
+		:pipe_(pipe)
+	{
+		pipe_->OnPacket(
+			[this](Packet&& packet)
+		{
+			InputStringArchive ar(std::string{ packet.Data(),packet.Size() });
+			auto tag = ar.Read<std::string>();
+			if (!tag)
+				return;
+
+			auto iter = listeners_.find(*tag);
+			if (iter == listeners_.end())
+				return;
+
+			auto content = ar.Read<std::string>();
+			if (!content)
+				return;
+
+			iter->second(std::move(*content));
+		});
+	}
+
+	template<typename Pipe>
+	void PlayerAgent<Pipe>::Listen(const std::string& tag, std::function<void(std::string&&)> handler)
+	{
+		assert(listeners_.find(tag) == listeners_.end());
+		listeners_[tag] = handler;
+	}
+}
---
+++ server/source/PlayerAgent_Test.cpp
@@ -0,0 +1,51 @@
+#ifdef CT_TEST
+#include <catch.hpp>
+#include "PlayerAgent.h"
+#include "../../common/source/Pipe.h"
+#include "../../common/source/Serializer.h"
+
+using namespace std::literals;
+
+namespace
+{
+	const std::string tag = "tag";
+	const std::string content = "content";
+	const std::string anotherTag = "tag2";
+}
+
+TEST_CASE("player agent has listener")
+{
+	auto pipe = std::make_shared<ct::MockPipe>();
+	ct::PlayerAgent agent{ pipe };
+	std::string received;
+	agent.Listen(tag, [&](std::string&& content)
+	{
+		received = content;
+	});
+
+	ct::OutputStringArchive ar;
+	ar.Write(tag);
+	ar.Write(content);
+	pipe->PacketArrive(ct::Packet(ar.String()));
+
+	REQUIRE(received == content);
+}
+
+TEST_CASE("player agent has no listener")
+{
+	auto pipe = std::make_shared<ct::MockPipe>();
+	ct::PlayerAgent agent{ pipe };
+	bool called = false;
+	agent.Listen(tag, [&](std::string&& content)
+	{
+		called = true;
+	});
+
+	ct::OutputStringArchive ar;
+	ar.Write(anotherTag);
+	ar.Write(content);
+	pipe->PacketArrive(ct::Packet(ar.String()));
+
+	REQUIRE(!called);
+}
+#endif
