3610d02c5b125c79f4a3ee0ccc4caea956a2e125
crazyZhang <917724035@qq.com>
2020-08-09 15:50:04 +0800

    split test executables to avoid test_case duplication

--- test/ClientLogin_Test.cpp
+++ test/ClientLogin_Test.cpp
@@ -1,3 +1,4 @@
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 #include "../client_logic/Login.h"
 #include "../common/Packet.h"
--- test/Server_Test.cpp
+++ test/Server_Test.cpp
@@ -1,3 +1,4 @@
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 #include "../server/Server.h"
 
