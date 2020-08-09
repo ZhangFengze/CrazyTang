89c904d77b4472c3e9ed5c9328985e4d11438a34
crazyZhang <917724035@qq.com>
2020-08-09 18:58:19 +0800

    client_cli.py test.py support change configuration

--- tools/client_cli.py
+++ tools/client_cli.py
@@ -5,15 +5,16 @@ from ct_common import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--clean", help="clean before build", action="store_true")
+parser.add_argument("--config", help="configuration", default="debug")
 args = parser.parse_args()
 
 source_dir = get_root_path().joinpath("client_cli")
 build_dir = get_root_path().joinpath("build/client_cli")
-output_path = build_dir.joinpath("Debug/client_cli")
+output_path = build_dir.joinpath(f"{args.config}/client_cli")
 
 if args.clean:
     rmdir(build_dir)
 
 cmake(source_dir, build_dir)
-build(build_dir, "debug")
+build(build_dir, args.config)
 execute(str(output_path))
--- tools/test.py
+++ tools/test.py
@@ -5,17 +5,18 @@ from ct_common import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--clean", help="clean before build", action="store_true")
+parser.add_argument("--config", help="configuration", default="debug")
 args = parser.parse_args()
 
 source_dir = get_root_path().joinpath("test")
 build_dir = get_root_path().joinpath("build/test")
-output_dir = build_dir.joinpath("Debug")
+output_dir = build_dir.joinpath(args.config)
 
 if args.clean:
     rmdir(build_dir)
 
 cmake(source_dir, build_dir)
-build(build_dir, "debug")
+build(build_dir, args.config)
 
 with os.scandir(output_dir) as it:
     for entry in it:
