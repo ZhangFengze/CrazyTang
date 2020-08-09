5ee85d6298865a0db9b00ba47baecb3a48ce6bbf
crazyZhang <917724035@qq.com>
2020-08-09 17:39:23 +0800

    run_all_tests.py use absolute path

--- run_all_tests.py
+++ run_all_tests.py
@@ -10,16 +10,21 @@ def print_colored(str):
 
 
 def execute(cmd):
-    print_colored("executing: {0}".format(cmd))
+    print_colored(f"executing: {cmd}")
     assert(0 == subprocess.call(cmd))
 
 
-execute("cmake -S test -B build/test")
-execute(
-    "cmake --build build/test --config debug -j {0}".format(os.cpu_count()))
+def get_root_path():
+    return os.path.dirname(os.path.abspath(__file__))
 
-dir = "./build/test/Debug"
-with os.scandir(dir) as it:
+
+test_dir = os.path.join(get_root_path(), "test")
+build_dir = os.path.join(get_root_path(), "build/test")
+
+execute(f"cmake -S {test_dir} -B {build_dir}")
+execute(f"cmake --build {build_dir} --config debug -j {os.cpu_count()}")
+
+with os.scandir(os.path.join(build_dir, "Debug")) as it:
     for entry in it:
         if entry.name.endswith('.exe') and entry.is_file():
             execute(entry.path)
