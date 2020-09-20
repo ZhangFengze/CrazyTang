4de5b4148e103c3d8dbe4dc9f09e430578dee99d
crazyZhang <917724035@qq.com>
2020-09-20 20:56:18 +0800

    Velocity

---
+++ common/Velocity.h
@@ -0,0 +1,22 @@
+
+#pragma once
+#include <Eigen/Eigen>
+namespace ct
+{
+    struct Velocity
+    {
+        Eigen::Vector3f data=Eigen::Vector3f::Zero();
+    };
+
+    template<typename OutputArchive>
+    void ArchiveVelocity(OutputArchive& ar, const Velocity& velocity)
+    {
+        ar.Write(velocity);
+    }
+
+    template<typename InputArchive>
+    void LoadVelocity(InputArchive& ar, Velocity& velocity)
+    {
+        ar.Read(velocity);
+    }
+}
