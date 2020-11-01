c0bf98e955258825ee2d3d6004655b79c2b0db46
crazyZhang <917724035@qq.com>
2020-11-01 12:01:20 +0800

    format code

--- server/Server.cpp
+++ server/Server.cpp
@@ -22,7 +22,7 @@ namespace ct
 	void Server::Run()
 	{
 		auto endpoint = asio::ip::tcp::endpoint{ asio::ip::make_address("127.0.0.1"),3377 };
-		ct::Acceptor acceptor{io_,endpoint,std::bind(&Server::OnConnection, this, _1, _2)};
+		ct::Acceptor acceptor{ io_,endpoint,std::bind(&Server::OnConnection, this, _1, _2) };
 
 		auto interval = std::chrono::milliseconds{ 33 };
 		auto shouldTick = std::chrono::steady_clock::now();
@@ -36,12 +36,12 @@ namespace ct
 		}
 	}
 
-	void Server::OnConnection(const std::error_code &error, asio::ip::tcp::socket &&socket)
+	void Server::OnConnection(const std::error_code& error, asio::ip::tcp::socket&& socket)
 	{
 		if (error)
 			return;
 		auto pipe = std::make_shared<Pipe<Socket>>(std::move(socket));
-		auto login = std::make_shared<Login<Pipe<Socket>>>(pipe, ++connectionID_, io_, std::chrono::seconds{3});
+		auto login = std::make_shared<Login<Pipe<Socket>>>(pipe, ++connectionID_, io_, std::chrono::seconds{ 3 });
 		login->OnSuccess(
 			[login, pipe, this](uint64_t id) {
 				OnLoginSuccess(pipe, id);
@@ -65,33 +65,33 @@ namespace ct
 		info->agent = agent;
 
 		agent->Listen("echo",
-					  [agent](std::string &&data) {
-						  agent->Send("echo", std::move(data));
-					  });
+			[agent](std::string&& data) {
+				agent->Send("echo", std::move(data));
+			});
 
 		agent->Listen("broadcast",
-					  [this, connectionID](std::string &&data) {
-						  OutputStringArchive out;
-						  out.Write(connectionID);
-						  out.Write(std::move(data));
-						  auto reply = out.String();
-						  for (auto &[_, other] : agents_)
-							  other->Send("broadcast", reply);
-					  });
+			[this, connectionID](std::string&& data) {
+				OutputStringArchive out;
+				out.Write(connectionID);
+				out.Write(std::move(data));
+				auto reply = out.String();
+				for (auto& [_, other] : agents_)
+					other->Send("broadcast", reply);
+			});
 
 		agent->Listen("list online",
-					  [this, agent](std::string &&data) {
-						  OutputStringArchive out;
-						  out.Write(agents_.size());
-						  for (auto &[id, _] : agents_)
-							  out.Write(id);
-						  agent->Send("list online", out.String());
-					  });
+			[this, agent](std::string&& data) {
+				OutputStringArchive out;
+				out.Write(agents_.size());
+				for (auto& [id, _] : agents_)
+					out.Write(id);
+				agent->Send("list online", out.String());
+			});
 
 		agent->OnError([e, connectionID, this]() mutable {
 			if (e.Valid())
 				e.Destroy();
 			agents_.erase(connectionID);
-		});
+			});
 	}
 } // namespace ct
