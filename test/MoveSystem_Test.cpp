8672ee2316426a352b90d5b67549079e694da2dc
crazyZhang <917724035@qq.com>
2020-11-01 10:44:42 +0800

    move system dummy test

--- test/MoveSystem_Test.cpp
+++ test/MoveSystem_Test.cpp
@@ -2,10 +2,17 @@
 #include <catch.hpp>
 #include "../common/Entity.h"
 #include "../common/MoveSystem.h"
+#include "../common/Position.h"
+#include "../common/Velocity.h"
 
 TEST_CASE("dummy")
 {
     ct::EntityContainer entities;
+    auto e = entities.Create();
+    auto position = e.Add<ct::Position>();
+    position->data = { 1.f,2.f,3.f };
+    auto velocity = e.Add<ct::Velocity>();
+    velocity->data = { 3.f,4.f,5.f };
     ct::move_system::Process(entities, 0.1f);
 }
 
