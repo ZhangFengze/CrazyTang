7a57874b2a7aba9ea0d8d0a0bafb3daa6a9a42de
crazyZhang <917724035@qq.com>
2020-11-07 20:45:10 +0800

    client_core build scripts

---
+++ tools/client_core.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+from ct_common import execute,get_root_path, rmdir, cmake, build
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--clean", help="clean before build", action="store_true")
+parser.add_argument("--config", help="configuration", default="debug")
+args = parser.parse_args()
+
+source_dir = get_root_path().joinpath("client_core")
+build_dir = get_root_path().joinpath("build/client_core")
+
+if args.clean:
+    rmdir(build_dir)
+
+cmake(source_dir, build_dir)
+build(build_dir, args.config)
