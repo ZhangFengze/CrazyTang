ca0bcb3208a3d6add4407129b30991b5875ee483
crazyZhang <917724035@qq.com>
2020-11-07 21:16:32 +0800

    client ue4

---
+++ client_ue4/CrazyTang/Source/CrazyTang/CrazyTang.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CrazyTang.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrazyTang, "CrazyTang" );
