6070e3312be7d903f48e8e0fcd4ce494746ab32b
crazyZhang <917724035@qq.com>
2021-05-16 22:24:58 +0800

    test/CMakeLists.txt install

--- tools/test.py
+++ tools/test.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from ct_common import execute, get_root_path, rmdir, cmake, build
+from ct_common import execute, get_root_path, rmdir, cmake, build, install
 
 
 parser = argparse.ArgumentParser()
@@ -10,16 +10,17 @@ parser.add_argument("--ninja", help="build using ninja", action="store_true")
 args = parser.parse_args()
 
 source_dir = get_root_path().joinpath("test")
-build_dir = get_root_path().joinpath("build/test")
-output_dir = build_dir.joinpath(args.config)
+build_dir = get_root_path().joinpath("build/build_test")
+install_dir = get_root_path().joinpath(f"build/test/{args.config}")
 
 if args.clean:
     rmdir(build_dir)
 
 cmake(source_dir, build_dir, args.ninja)
 build(build_dir, args.config)
+install(build_dir, install_dir, args.config)
 
-with os.scandir(output_dir) as it:
+with os.scandir(install_dir.joinpath("bin")) as it:
     for entry in it:
         if entry.name.endswith('.exe') and entry.is_file():
             execute(entry.path)
