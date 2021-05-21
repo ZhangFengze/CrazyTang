87d19ecfbcb95da86ff55b6578feb2c536a5868f
crazyZhang <917724035@qq.com>
2021-05-21 23:07:12 +0800

    client_gui login success log

--- client_gui/client_gui.cpp
+++ client_gui/client_gui.cpp
@@ -48,8 +48,7 @@ namespace
         auto id = co_await ClientLogin(s, std::chrono::seconds{ 3 });
         if (!id)
             co_return;
-        Log("login success");
-        printf("login success %llu\n", *id);
+        Log("login success "+std::to_string(*id));
         auto agent = std::make_shared<NetAgent>(std::move(s));
         agent->OnError(
             [agent]() {
